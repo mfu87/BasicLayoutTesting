f78d5fb7b6fe803f39317af05fbf7c1e13d8ed48
Marcus Fuchs <mfu.chs@icloud.com>
2019-08-18 10:54:05 +0200

    Initial Commit

---
+++ BasicLayoutTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicLayoutTesting
+//
+//  Created by Marcus Fuchs on 18.08.19.
+//  Copyright © 2019 Marcus Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
